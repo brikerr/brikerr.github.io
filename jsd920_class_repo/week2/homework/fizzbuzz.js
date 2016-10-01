1cb3b9e16e3dfadff82f83ef70f79a8157ce1f8c
bkerr <bjk@Brians-MacBook-Pro.local>
2016-10-01 17:05:12 -0400

    Added FizzBuzz homework

--- jsd920_class_repo/week2/homework/fizzbuzz.js
+++ jsd920_class_repo/week2/homework/fizzbuzz.js
@@ -1,3 +1,22 @@
+// FIZZBUZZ
+// Using loops, boolean logic, and if/else statements, incrementally build the common fizzbuzz loop.
+
+// - In the loop, every time a number is divisible by **3**, instead of logging the number itself, the word "fizz" should appear.
+// - If the number is divisible by  **5**, the word "buzz" should be logged.
+// - If the number is divisible by both **3** and  **5**, then the word "fizzbuzz" should be logged.
+//
+// EX:
+// 1
+// 2
+// 'fizz'
+// 4
+// 'buzz'
+// 'fizz'
+// 7
+// 8
+// 'fizz'
+// 'buzz'
+
 for (var number = 0; number <= 100; number = number + 1)
 
 	if (number % 3 == 0 && number % 5 != 0) console.log("Fizz");
