f96c1d908c3fbeb4c131244df8d7c7e94257a0ea
bkerr <bjk@Brians-MacBook-Pro.local>
2016-10-01 16:26:18 -0400

    adding FizzBuzz homework

--- jsd920_class_repo/week2/homework/fizzbuzz.js
+++ jsd920_class_repo/week2/homework/fizzbuzz.js
@@ -1,36 +1,7 @@
-// FIZZBUZZ
-// Using loops, boolean logic, and if/else statements, incrementally build the common fizzbuzz loop.
+for (var number = 0; number <= 100; number = number + 1)
 
-// - In the loop, every time a number is divisible by **3**, instead of logging the number itself, the word "fizz" should appear.
-// - If the number is divisible by  **5**, the word "buzz" should be logged.
-// - If the number is divisible by both **3** and  **5**, then the word "fizzbuzz" should be logged.
-//
-// EX:
-// 1
-// 2
-// 'fizz'
-// 4
-// 'buzz'
-// 'fizz'
-// 7
-// 8
-// 'fizz'
-// 'buzz'
-
-// ##### Step 1:
-// Construct a for loop that iterates through, and `console.log()`'s out, numbers 1 - 100.
-
-
-
-// ##### Step 2:
-// Add an `if/else` statement that logs the string `"fizz"` if the value being iterated over is divisible by `3`; otherwise, log out the value.
-
-
-
-// ##### Step 3:
-// Add an `else if` clause that logs the string `"buzz"` if the value being iterated over is divisible by `5`.
-
-
-
-// ##### Step 4:
-// Add an additional `else if` clause that logs the string `"fizzbuzz"` if the value being iterated over is divisible by both `3` and `5`.
+	if (number % 3 == 0 && number % 5 != 0) console.log("Fizz");
+	else if (number % 5 == 0 && number % 3 != 0) console.log("Buzz");
+	else if (number % 3 == 0 && number % 5 == 0) console.log("FizzBuzz");
+	else if (number % 2 == 0 && number % 4 == 0 && number % 8 == 0) console.log("ExtraCredit");
+	else console.log(number);
