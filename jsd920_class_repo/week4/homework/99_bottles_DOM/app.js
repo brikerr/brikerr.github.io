eee5d61881a23c6665f132515c71481440c6eaec
bkerr <bjk@Brians-MBP.fios-router.home>
2016-10-16 10:03:28 -0400

    adding 99 Bottles/DOM homework

--- jsd920_class_repo/week4/homework/99_bottles_DOM/app.js
+++ jsd920_class_repo/week4/homework/99_bottles_DOM/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $("#beer").before("<h3>Lots of Drinkin'</h3>");
     for (var bottles = 99; bottles >= 2; bottles = bottles - 1)
-    $("#beer").append("<li>" + bottles + " bottles of beer on the wall, " + bottles + " bottles of beer. Take one down and pass it around, " + (bottles - 1) + " bottles of beer on the wall.</li> </br>");
-    $("#beer").append("<li>" + bottles + " bottle of beer on the wall, " + bottles + " bottle of beer. Take one down and pass it around, " + (bottles - 1) + " bottle of beer on the wall.</li> </br>");
-    $("#beer").append("<li> No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall. </li> </br>");
+    $("#beer").append("<li>" + bottles + " bottles of beer on the wall, " + bottles + " bottles of beer. </br>Take one down and pass it around, " + (bottles - 1) + " bottles of beer on the wall.</li> </br>");
+    $("#beer").append("<li>" + bottles + " bottle of beer on the wall, " + bottles + " bottle of beer. </br>Take one down and pass it around, " + (bottles - 1) + " bottle of beer on the wall.</li> </br>");
+    $("#beer").append("<li> No more bottles of beer on the wall, no more bottles of beer. </br>Go to the store and buy some more, 99 bottles of beer on the wall. </li> </br>");
     $("#beer").after("<h3>Prost!</h3>");
 });
