d1c3d6b03b19a199fc889aa4056919c6b0b0affc
bkerr <bkerr79@gmail.com>
2016-10-29 18:04:01 -0400

    adding project folder

---
+++ jsd920_class_repo/projects/feedr/js/app.js
@@ -0,0 +1,32 @@
+/*
+  Please add all Javascript code to this file.
+*/
+
+$.get("https://accesscontrolalloworiginall.herokuapp.com/http://mashable.com/stories.json", function(results) {
+    //an array of articles objects
+    var articles = results.new;
+    var articlesCollection = [];
+    articles.forEach(function(article) {
+        var singleArticleObj = {
+            title: article.title,
+            link: article.link,
+            feature_image_src: article.feature_image,
+            channel: article.channel,
+            total_shares: article.shares.total
+        };
+        //add singleArticleObj to articlesCollection
+        articlesCollection.push(singleArticleObj);
+    });
+    console.log(articlesCollection);
+
+    //1. grab handblebars template
+    var articleTemplate = $("#article-template").html();
+    //2. complie template
+    var compileTemp = Handlebars.compile(articleTemplate);
+    //3. add objs
+    var articlesHtmlBlock = compileTemp({
+      articles: articlesCollection
+    });
+    //4. append
+    var hbList = $('#all_articles').append(articlesHtmlBlock);
+});
